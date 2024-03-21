044ca0f6c7764668a38345ee84e54333d5ce2d72
Diogo Deconto <dideconto@gmail.com>
2024-03-21 09:25:51 -0300

    Adicionando projeto de Ambientação e da Lista de Exercício

---
+++ ListaExercicio/Exe06A/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Clear();
+//Criar um vetor
+int[] vetor = new int[100];
+
+//Criar o objeto que vai gerar o número randomico
+Random aleatorio = new Random();
+
+//Preencher vetor com valores aleatórios
+for (int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = aleatorio.Next(100);
+}
+
+//Imprimir vetor
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+//Ordenar vetor
+bool troca = false;
+do
+{
+    troca = false;
+    for (int i = 0; i < vetor.Length - 1; i++)
+    {
+        if (vetor[i] > vetor[i + 1])
+        {
+            int aux = vetor[i];
+            vetor[i] = vetor[i + 1];
+            vetor[i + 1] = aux;
+            troca = true;
+        }
+    }
+} while (troca);
+
+Console.WriteLine("\n");
+
+//Imprimir vetor
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
---
+++ ListaExercicio/Exe06B/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exe06B;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        int[] vetor = new int[100];
+        Random aleatorio = new Random();
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = aleatorio.Next(100);
+        }
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+        //Ordenação com alguma funcionalidade da linguagem
+        Array.Sort(vetor);
+
+        Console.WriteLine("\n");
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+    }
+    
+}
