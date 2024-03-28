48d5592bd21d08f8c73ceb36d5f7804ea1194941
Diogo Deconto <dideconto@gmail.com>
2024-03-28 11:52:03 -0300

    API - Listagem e busca com parâmetro

---
+++ API/API/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿namespace API.Models;
+
+public class Produto
+{
+    //Construtor
+    public Produto() { }
+
+    public Produto(string nome, string descricao, double valor)
+    {
+        Nome = nome;
+        Descricao = descricao;
+        Valor = valor;
+    }
+
+    //Atributos ou propriedades = Características de um objeto
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public double Valor { get; set; }
+
+    // private string nome;
+    // public void setNome(string nome)
+    // {
+    //     this.nome = nome;
+    // }
+    // public string getNome()
+    // {
+    //     return this.nome;
+    // }
+}
--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,20 +1,50 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Produto> produtos = new List<Produto>();
-produtos.Add(new Produto("Celular", "IOS"));
-produtos.Add(new Produto("Celular", "Android"));
-produtos.Add(new Produto("Televisão", "LG"));
-produtos.Add(new Produto("Placa de Vídeo", "NVIDIA"));
+List<Produto> produtos =
+[
+    new Produto("Celular", "IOS", 5000),
+    new Produto("Celular", "Android", 4000),
+    new Produto("Televisão", "LG", 2300.5),
+    new Produto("Placa de Vídeo", "NVIDIA", 2500),
+];
 
 //Funcionalidades da aplicação - EndPoints
+
+// GET: http://localhost:5124/
+app.MapGet("/", () => "API de Produtos");
+
+// GET: http://localhost:5124/produto/listar
 app.MapGet("/produto/listar", () =>
     produtos);
 
-//EXERCÍCIO - Cadastrar produtos dentro da lista
+// GET: http://localhost:5124/produto/buscar/nomedoproduto
+app.MapGet("/produto/buscar/{nome}", ([FromRoute] string nome) =>
+    {
+        for (int i = 0; i < produtos.Count; i++)
+        {
+            if (produtos[i].Nome == nome)
+            {
+                //retornar o produto encontrado
+                return Results.Ok(produtos[i]);
+            }
+        }
+        return Results.NotFound("Produto não encontrado!");
+    }
+);
+
+// POST: http://localhost:5124/produto/cadastrar
 app.MapPost("/produto/cadastrar", () =>
     "Cadastro de produtos");
 
-app.Run();
+//EXERCÍCIOS
+//1) Cadastrar um produto 
+//a) Pela URL
+//b) Pelo corpo
+//2) Remoção do produto
+//3) Alteração do produto
 
-record Produto(string nome, string descricao);
+app.Run();
