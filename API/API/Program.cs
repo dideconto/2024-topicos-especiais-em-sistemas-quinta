1c7e9880d06ec73dc406e40926d0b44abd2bf57e
Diogo Deconto <dideconto@gmail.com>
2024-04-04 10:39:47 -0300

    API - Cadastro do produto pelo corpo da requisição

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -38,18 +38,8 @@ app.MapGet("/produto/buscar/{nome}", ([FromRoute] string nome) =>
 );
 
 // POST: http://localhost:5124/produto/cadastrar
-app.MapPost("/produto/cadastrar/{nome}/{descricao}/{valor}",
-    ([FromRoute] string nome, [FromRoute] string descricao,
-    [FromRoute] double valor) =>
+app.MapPost("/produto/cadastrar", ([FromBody] Produto produto) =>
 {
-    //Preencher o objeto pelo construtor
-    Produto produto = new Produto(nome, descricao, valor);
-
-    //Preencher o objeto pelos atributos
-    produto.Nome = nome;
-    produto.Descricao = descricao;
-    produto.Valor = valor;
-
     //Adicionar o objeto dentro da lista
     produtos.Add(produto);
     return Results.Created("", produto);
