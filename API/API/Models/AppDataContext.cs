5fd7129f38a227787166018ad4cb0e113542fb64
Diogo Deconto <dideconto@gmail.com>
2024-04-11 11:46:43 -0300

    API - Configuração com o banco de dados

---
+++ API/API/Models/AppDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+//Classe que representa Entity Framework Core : Code First
+public class AppDataContext : DbContext
+{
+    //Representação das classes que vão virar
+    //tabelas do banco de dados
+    public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //Configuração da conexão com o banco de dados
+        optionsBuilder.UseSqlite("Data Source=app.db");
+    }
+}
